e74263a8f0b211ada4b98a722940e30c38720a83
Johann six <six.johann@gmail.com>
2026-03-24 09:58:08 +0100

    add gob utility and publish gob

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -1,7 +1,9 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -17,4 +19,35 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		Body:        body,
 	})
 	return nil
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	body, err := encodeGob(val)
+	if err != nil {
+		return err
+	}
+	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/gob",
+		Body:        body,
+	})
+	return nil
+}
+
+func encodeGob[T any](val T) ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	if err := encoder.Encode(val); err != nil {
+		return buf.Bytes(), err
+	}
+	return buf.Bytes(), nil
+}
+
+func decodeGob[T any](data []byte) (T, error) {
+	var val T
+	buf := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buf)
+	if err := decoder.Decode(&val); err != nil {
+		return val, err
+	}
+	return val, nil
 }
